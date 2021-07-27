96207894df67f75bf19ba9aae667afc04a0da6e5
BishalRaj <bisal1shakya@gmail.com>
2021-07-27 15:59:57 +0545

    upcoming sale edited

--- extra/upcoming-sale.js
+++ extra/upcoming-sale.js
@@ -1,19 +1,21 @@
 function callUpcomingCard() {
   var card = `
-  <div class="col-lg-6 col-md-6 col-sm-12 col-12">
+  <div class="col-lg-5 col-md-5 col-sm-12 col-12">
   <div class="row m-2 shadow-lg rounded">
-      <div class="col-5 m-auto"> <img
+      <div class="col-6 m-auto"> <img
               src="https://img.freepik.com/free-photo/excited-white-girl-bright-stylish-glasses-posing-pink-dreamy-curly-woman-playing-with-her-ginger-hair-laughing_197531-11045.jpg?size=626&ext=jpg"
-              class="card-img-top rounded-top" alt="..."></div>
-      <div class="col-7 w-100">
+              class="card-img-top rounded-top" alt="...">
+      </div>
+
+      <div class="col-6 w-100">
           <div class="w-100 p-2">
               <h5 class="card-title mb-0 pb-0">Shirt</h5>
               <p class="card-text mt-0 mb-2"><small>Abc Shop</small></p>
 
               <!-- Price -->
               <div class="w-100 d-flex">
-                  <p class="w-25 p-0 mr-3 text-grey"> <del>Rs 85000</del> </p>
-                  <p class="w-50  p-0 m-0 text-price"> <b>Rs 75000</b> </p>
+              <p class="p-0 mr-3 text-grey"> <del>Rs 85000</del> </p>
+              <p class="p-0 m-0 text-price"> <b>Rs 75000</b> </p>
               </div>
 
 
